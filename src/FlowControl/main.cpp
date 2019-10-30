c4bfbe1d742d393fcdcdf7ad06d0143089aca978
Taras Barninets <barninets@gmail.com>
2019-10-31 00:54:26 +0200

    Implement example of fall through in switch statement
    Change-Id: I22b8d1580c7dd0896dec2de50d94ed5884eb434a

---
+++ src/FlowControl/main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+int main()
+{
+	int a, b;
+	std::cin >> a >> b;
+
+	short operation;
+	std::cin >> operation;
+	
+	switch (operation) // operation allows only INTEGER numbers(char, short, int)
+	{
+	case 1: 
+		std::cout << "a + b = " << a + b << std::endl;
+		break;
+	case 2: 
+		std::cout << "a - b = " << a - b << std::endl;
+		break;
+	case 3:
+		std::cout << "a * b = " << a * b << std::endl;
+		// this is error, should be break, otherwise occurs fall through
+	case 4:
+		std::cout << "a / b = " << a / b << std::endl;
+		break;
+	default:
+		std::cout << "enter correct operation" << std::endl;
+		break;
+	}
+
+	do
+	{
+		std::cout << "Before continue" << std::endl;
+		continue; // jump to the begin of the next iteration
+		std::cout << "After continue" << std::endl;
+	} while (true);
+
+	return EXIT_SUCCESS;
+}
