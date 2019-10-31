e14921a79316622af2af0c2ea7a944f2fa309a71
Taras Barninets <barninets@gmail.com>
2019-11-01 00:44:27 +0200

    Implement example of using bitwise operations and bitmask
    Change-Id: I70407166e2bbf25d8617200b2e565389b2a7ee26

--- src/DataTypes/Main.cpp
+++ src/DataTypes/Main.cpp
@@ -2,7 +2,7 @@
 
 void print_memory(unsigned char* memory, size_t bytes)
 {
-	for (int i = 0; i < bytes; i++)
+	for (size_t i = 0; i < bytes; i++)
 	{
 		std::cout << std::hex << static_cast<unsigned short>(*(memory + i)) << " ";
 	}
@@ -79,6 +79,42 @@ int main()
 	// C++11
 	Currency current = Currency::UAH;
 	test(static_cast<int>(current)); // can not convert Currency to int
+
+	// bitwise operations and two compliment code
+	int i = 8; // 0000 0000 0000 0000 0000 0000 0000 1000 - direct code
+	i = 8 | 1; // 0000 0000 0000 0000 0000 0000 0000 1001 - direct code
+	std::cout << i; // 9
+
+	i = ~i; // 1111 1111 1111 1111 1111 1111 1111 0110 - two compliment code
+			// 0000 0000 0000 0000 0000 0000 0000 1001 - reverse code
+			// 0000 0000 0000 0000 0000 0000 0000 1010 - +1 -> direct code
+			// result = - direct code = - 10
+
+	std::cout << i; // - 10
+
+	unsigned int k = 11; // 0000 0000 0000 0000 0000 0000 0000 1011, 0000 0000 0000 0000 0000 0000 0000 0100
+	k = k ^ 4; // 0000 0000 0000 0000 0000 0000 0000 1111
+	std::cout << k << std::endl; // 15
+
+	int q = k; // 0000 0000 0000 0000 0000 0000 0000 1111
+	std::cout << q << std::endl; // 15
+
+	k = k | 0b1000'0000'0000'0000'0000'0000'0000'0000; // 1000 0000 0000 0000 0000 0000 0000 1111
+	q = k; // 1000 0000 0000 0000 0000 0000 0000 1111 , MSB is - 1, we understand this bits are two compliment code
+	// 0111 1111 1111 1111 1111 1111 1111 0000 , ~ - opposite code
+	// 0111 1111 1111 1111 1111 1111 1111 0001 , +1 - direct code
+	// result = - direct code = - 2147483633
+	std::cout << q << std::endl;
+
+	// example of using bitmask
+	const unsigned int SECOND_BYTE_MASK = 0x0000FF00;
+	unsigned int num = 0xABCD10EF;
+	unsigned int intermid = num & SECOND_BYTE_MASK; // 0x00001000
+	unsigned char data = static_cast<unsigned char>(intermid >> 8); // 0x00000010 , logical shift because UNSIGINED value
+
+	// example of arithmetical shift
+	int value = -41561616; // 0b1111'1101'1000'0101'1101'0001'1111'0000 - 0xFD85D1F0
+	value = value >> 2; // 0b1111'1111'0110'0001'0111'0100'0111'1100 - 0xFF61747C
 
 	return EXIT_SUCCESS;
 }
