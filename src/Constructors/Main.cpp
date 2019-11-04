1705ca8546377a4b6ff31ad221d0f7f7007e4a8a
Taras Barninets <barninets@gmail.com>
2019-11-05 01:45:37 +0200

    Implement overloading of operators +,-,++,--,<< for Point class
    Change-Id: I6433221d1fa41793d8ab683702a4225c1c2bccf7

--- src/Constructors/Main.cpp
+++ src/Constructors/Main.cpp
@@ -25,6 +25,22 @@ int main()
 	second_copy_rect.printColor(); 
 	copy_rect.setColor(2, 2, 2);
 	second_copy_rect.printColor(); // still 10, 125, 200 because second_copy_rect has own instance of Color
+	std::cout << std::endl;
+
+	Point q(20, 6);
+	Point w(10, 4);
+	Point t = q + w;
+	std::cout << q << " + " << w << " = " << t << std::endl; 
+	Point m = q - w;
+	std::cout << q << " - " << w << " = " << m << std::endl;
+	std::cout << "Prefix ++" << q << " = ";
+	std::cout << ++q << std::endl;
+	std::cout << "Prefix --" << w << " = ";
+	std::cout << --w << std::endl;
+	std::cout << "Postfix ++" << q << " = ";
+	std::cout << q++ << std::endl;
+	std::cout << "Postfix --" << w << " = ";
+	std::cout << w-- << std::endl;
 
 	return EXIT_SUCCESS;
 }
--- src/Constructors/Point.h
+++ src/Constructors/Point.h
@@ -9,6 +9,13 @@ public:
 	Point(int x);
 	Point& operator= (const Point& rhs);
 
+	friend Point operator+(const Point &point1, const Point &point2); //overloaded operator + via friend function
+	friend std::ostream& operator<< (std::ostream& out, const Point& point); //overloaded operator << via friend function
+	Point& operator++(); //overloaded prefix operator ++() via class method
+	Point& operator--(); //overloaded prefix operator --() via class method
+	Point operator++(int); //overloaded postfix operator ++() via class method
+	Point operator--(int); //overloaded postfix operator --() via class method
+
 	int getX() const;
 	int getY() const;
 	void setX(int x);
@@ -17,3 +24,5 @@ public:
 private:
 	int m_x, m_y;
 };
+
+Point operator-(const Point& lhs_point, const Point& rhs_point); //overloaded operator - via function
