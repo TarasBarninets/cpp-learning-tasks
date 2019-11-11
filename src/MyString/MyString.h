4c80219787bcf4717b3b52222752d74cd24e1774
Taras Barninets <barninets@gmail.com>
2019-11-12 00:04:14 +0200

    Split MyString implementation into .cpp and .h files
    Change-Id: I7d49d386fc15979df596db10d871a80c6a05c56b

---
+++ src/MyString/MyString.h
@@ -0,0 +1,29 @@
+#pragma once
+
+class MyString
+{
+public:
+
+	MyString();
+	MyString(const char* c_str);
+	MyString(const MyString& str);
+	~MyString();
+
+	MyString& operator=(const MyString& rhs);
+	char& operator[](size_t index) const;
+	bool operator==(const MyString& rhs);
+	MyString operator+(const MyString& rhs);
+	MyString& const operator+=(const MyString& rhs);
+
+	size_t length() const;
+	bool empty() const;
+	void clear();
+	size_t find(const MyString& str) const;
+	void swap(MyString& str);
+	const char* c_str() const;
+
+private:
+	char* m_data = nullptr;
+	size_t m_size = 0;
+	static const size_t m_npos = -1;
+};
